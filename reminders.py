49a8293c947d34c4d30fa61753978f4d85a9ccf2
vagrant <vagrant@vagrant.vm>
2017-10-09 19:55:11 +0000

    started sceduling reminders using scedule module

---
+++ reminders.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+
+def send_message():
+
+    print "git commit"
+
+# schedule.every().day.at("10:30").do(send_message)
+schedule.every(15).minutes.do(send_message)
+
+if __name__ == "__main__":
+    While True:
+        schedule.run_pending()
+        time.sleep(1)
