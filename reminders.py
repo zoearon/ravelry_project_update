6bcd2ad44111ed78bc41d8fcfa98904898434d06
vagrant <vagrant@vagrant.vm>
2017-10-10 00:41:14 +0000

    trying to get a function to find num of projects needing updates

--- reminders.py
+++ reminders.py
@@ -25,6 +25,15 @@ def send_text():
 
     print(message.sid)
 
+def message():
+    """ Create mesage from database info """
+
+    update_freqs = db.session.query(User.update_time, Project).join(
+                                   Project).filter(Project.status_id == 1,
+                                                   Project.updated_at < (
+                                                    now - datetime.timedelta(days=User.update_time))).all()
+
+    mess = "%s projects haven't been updated in %s days!" % count, freq
 # schedule.every().day.at("10:30").do(send_message)
 schedule.every(15).minutes.do(send_text)
 
